1bd55e5990dfb58b15c85a565222a8fcb3c00cb1
Chase Grijalva, Joe Parsley and Michelle Poterek <michelle@>
2017-05-02 15:09:56 -0700

    working word challenge project

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,18 @@
 //   event.preventDefault();
 //   });
 // });
+$(document).ready(function() {
+    $("#form").submit(function(event) {
+    event.preventDefault();
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    var sentence = $("#sentence-input").val();
+    var newSentence = sentence;
+    for (var index = 0; index < vowels.length; index++) {
+      newSentence = newSentence.split(vowels[index]).join("-");
+    }
 
-var vowels = ['a', 'e', 'i', 'o', 'u'];
-var sentence = "here we go";
-var newSentence = sentence
-for (var index = 0; index < vowels.length; index++) {
-  newSentence = newSentence.split(vowels[index]).join("-");
+    // $("#button").click(function() {
+    $(".output").text(newSentence);
 
-}
+  });
+});
