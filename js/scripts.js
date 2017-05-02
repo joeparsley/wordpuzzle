50a7bdbb58bf961211131dfa15ba9dc2c13cc913
Chase Grijalva <chasepgrijalva@gmail.com>
2017-05-02 14:25:37 -0700

    final save before commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-$(document).ready(function() {
-  var sentence = document.getElementById("sentence").innerHTML;
-  console.log(sentence);
-  var noVowels = sentence.replace(/[aAeEiIoOuU]/ig," _ ");
-  console.log(noVowels);
+// $(document).ready(function() {
+//   var sentence = document.getElementById("sentence").innerHTML;
+//   console.log(sentence);
+//   var noVowels = sentence.replace(/[aAeEiIoOuU]/ig," _ ");
+//   console.log(noVowels);
+//
+//   $("span#challenge").append(noVowels);
+//
+//   $("#button").click(function() {
+//   $("#challenge").toggle();
+//   $("#sentence").toggle();
+//
+//   event.preventDefault();
+//   });
+// });
 
-  $("span#challenge").append(noVowels);
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var sentence = "here we go";
+var newSentence = sentence
+for (var index = 0; index < vowels.length; index++) {
+  newSentence = newSentence.split(vowels[index]).join("-");
 
-  $("#button").click(function() {
-  $("#challenge").toggle();
-  $("#sentence").toggle();
-
-  event.preventDefault();
-  });
-});
+}
